14b5c2d189d5feface2a8d50fd80a001b7342bea
Tuan <naroto102hn2@gmail.com>
2020-12-15 22:50:31 +0700

    done w13

--- week13/hw2.c
+++ week13/hw2.c
@@ -1,25 +1,67 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-void trimMiddle(char a[]){
-  int n = strlen(a);
-  int i,j;
-  for(i=0;i<n;i++){
-    if(a[i]==' ' && a[i+1]==' '){
-      for(j=i+1;j<n;j++){
-	a[j]=a[j+1];
-      }
-      i--;
-      n--;
+
+void strdelete(char *s,int vt,int sl)
+    {  
+        int n=strlen(s),i;
+        for (i=vt;i<=n-sl;++i)
+            s[i]=s[i+sl];
     }
+void chuanhoachuoi(char a[])
+    {
+        int i;
+	int j=0;
+	
+        while(a[strlen(a)-1]==' ')// xoa het cac ki tu trong o cuoi chuoi
+            a[strlen(a)-1]=0;
+        while(a[0]==' ')          //xoa het cac ki tu trong o dau chuoi
+            strdelete(a,0,1);
+        for(i=0;i<strlen(a);++i)
+            if((a[i]>=65)&&(a[i]<=90))  //kiem tra co phai la ky tu hoa
+                a[i]+=32;        //chuyen tat ca ve ky tu thuong
+     
+        i=0;
+        while(i<strlen(a))                //xoa hai ki tu trong lien tiep
+         if((a[i]==' ')&&(a[i+1]==' '))
+             strdelete(a,i,1);
+         else ++i;
+     
+       for(i=0;i<strlen(a);i++)
+        if(a[i]==' ')
+            a[i+1]-=32;  //chuyen chu cai sau khoang trang thanh chu hoa
+       
+       a[0]=toupper(a[0]);//chuyen chu cai dau tien thanh chu hoa
+         
+       
+        i=0;
+        while(i<strlen(a))    //xoa cac ki tu khong phai la chu cai
+        if((a[i]!=' ')&&(!isalpha(a[i])))
+            strdelete(a,i,1);
+         else ++i;
+	
+    }
+
+void nhap(char a[], int n){
+  int i;
+  for(i=0;i<n;i++){
+    
+    printf("Nhap ten hs %d: ",i+1);
+    //while((getchar())!='\n');
+    gets(a);
   }
 }
 
 int main()
-{
-  char s[50]= "     Kieu   Anh Tuan      .";
-  printf("%s\n",s);
-  trimMiddle(s);
-  printf("%s\n",s);
-  return 0;
-}
+    {
+       char a[100];
+   
+
+	 nhap(a,5);
+       chuanhoachuoi(a);
+       printf("Chuoi sau chuan hoa: %s\n",a);
+       return 0;
+    }
+     
+   
--- week13/hw3.c
+++ week13/hw3.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-void encode(char s[]){
+void encode(char s[],int k){
   int i;
   int n = strlen(s);
   for(i=0;i<n;i++){
-    if(s[i]>='y') s[i]=s[i]-24;
+    if(s[i]>='z'-k+1) s[i]=s[i]-26+k;
     else{
-      s[i]=s[i]+2;
+      s[i]=s[i]+k;
     }
   }
 }
 
-void decode(char s[]){
+void decode(char s[],int k){
   int i;
   int n = strlen(s);
   for(i=0;i<n;i++){
-    if(s[i]<='b') s[i] = s[i]+24;
+    if(s[i]<='a'+k-1) s[i] = s[i]+26-k;
     else{
-      s[i]=s[i]-2;
+      s[i]=s[i]-k;
     }
   }
 }
@@ -26,10 +26,13 @@ void decode(char s[]){
 int main()
 {
   char s[30];
+  int k;
   scanf("%s",s);
-  encode(s);
-  printf("%s\n",s);
-  decode(s);
-  printf("%s\n",s);
+  printf("Nhap so buoc nhay: ");
+  scanf("%d",&k);
+  encode(s,k);
+  printf("Chuoi sau khi ma hoa: %s\n",s);
+  /*decode(s,k);
+    printf("Chuoi sau khi giai ma:%s\n",s);*/
   return 0;
 }
